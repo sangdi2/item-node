95fcf60fa5f39a3f1f5429b86707722f54a57627
sangdi <1592807515@qq.com>
2022-03-01 15:13:41 +0800

    rdfg

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ app.use(cors())
 
 app.use(express.urlencoded({extended:false}))
 
+app.use(function(req,res,next){
+    res.cc=function(err,status=0){
+        res.send({
+            status,
+            message:err instanceof Error?err.message:err
+        })
+    }
+    next()
+})
+
 const rt =require('./router/user')
 
 app.use('/api',rt)
--- router_handler/user.js
+++ router_handler/user.js
@@ -1,5 +1,32 @@
 exports.register=function(req,res){
-    res.send('注册成功')
+    const userinfo =req.body
+    const db =require('../db/index')
+    const mysql='select * from users where username=?'
+    const bcryptjs =require('bcryptjs')
+    const update ='insert into users set ?'
+    if(!userinfo.username||!userinfo.password)
+    {
+        return res.cc('用户名或密码不允许为空!')
+    }
+    db.query(mysql,userinfo.username,function(err,results){
+        if(err){
+            return res.cc(err.message)
+        }
+        if(results.length>0){
+            return res.cc('用户名被占用!')
+        }
+    })
+    userinfo.password=bcryptjs.hashSync(userinfo.password,10)
+    db.query(update,{username:userinfo.username,password:userinfo.password},function(err,results){
+        if(err){
+            return res.cc(err.message)
+        }
+        if(results.affectedRows !== 1){
+            return res.send({status:1,message:'插入用户成功'})
+        }
+        res.cc('插入用户成功',1)
+    })
+    
 }
 
 exports.login=function(req,res){
